4961cab1384a96e381eeaf33a01cf7c798dee6dd
sinhvien <sinhvien>
2021-03-30 10:04:46 +0700

    secord commit

---
+++ phamnguyenmanhtruong/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(phamnguyenmanhtruong.Startup))]
+namespace phamnguyenmanhtruong
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
